2bebdbabd6fbdb7a14420c2b148ee7513dce25f4
messengerb043 <messengerb043@gmail.com>
2025-06-26 19:19:53 +0600

    Update Timkiem.js

--- Script/commands/Timkiem.js
+++ Script/commands/Timkiem.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "search",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "EMON SHEIKH❣️",
     description: "Search results on google",
     commandCategory: "info",
     usages: "search [Text]",
