34383b3dfaa91c0090b88bce53ebe042abf749ee
messengerb043 <messengerb043@gmail.com>
2025-06-26 23:00:55 +0600

    Update Google.js

--- Script/commands/Google.js
+++ Script/commands/Google.js
@@ -5,7 +5,7 @@ module.exports.config = {
 
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "[ʜᴀꜱɪʙᴜʟ ɪꜱʟᴀᴍ ᴇᴍᴏɴ]",
 	description: "Comment on table ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "google [text]",
