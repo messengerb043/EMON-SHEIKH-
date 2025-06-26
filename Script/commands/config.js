d0760484d895086295ba956543baf28c935f5d2a
messengerb043 <messengerb043@gmail.com>
2025-06-26 22:58:43 +0600

    Update config.js

--- Script/commands/config.js
+++ Script/commands/config.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "config",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "[ʜᴀꜱɪʙᴜʟ ɪꜱʟᴀᴍ ᴇᴍᴏɴ]",
 	description: "config bot!",
 	commandCategory: "admin",
 	cooldowns: 5
