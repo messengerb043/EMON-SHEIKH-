1eba5b1d5601c43af4f8c0b5c46ded8d14654c2e
messengerb043 <messengerb043@gmail.com>
2025-06-27 22:03:58 +0600

    Update acp.js

--- Script/commands/acp.js
+++ Script/commands/acp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "acp",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 𝑻𝑬𝑨𝑴_ ☢️",
   description: "Make friends via Facebook id",
   commandCategory: "bot id",
   usages: "uid",
