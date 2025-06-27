354e907bcefbe8bcbeeb663f90da4749f6d2c61b
messengerb043 <messengerb043@gmail.com>
2025-06-27 22:11:52 +0600

    Update Work-beta.js

--- Script/commands/Work-beta.js
+++ Script/commands/Work-beta.js
@@ -3,7 +3,7 @@ module.exports.config = {
     name: "job",
     version: "1.0.2",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
+    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
     description: "",
     commandCategory: "Economy",
     cooldowns: 5,
