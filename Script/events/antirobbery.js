dc9c161cec873d7990ae4c7d5406196fa49752fa
messengerb043 <messengerb043@gmail.com>
2025-06-27 15:45:05 +0600

    Update antirobbery.js

--- Script/events/antirobbery.js
+++ Script/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "[ʜᴀꜱɪʙᴜʟ ɪꜱʟᴀᴍ ᴇᴍᴏɴ]",
     description: "Prevent admin changes",
 };
 
