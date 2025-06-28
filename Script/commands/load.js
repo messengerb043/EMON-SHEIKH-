1335a1a8880e7d06c0ab3f78a2f1f269b2f77119
messengerb043 <messengerb043@gmail.com>
2025-06-29 05:39:06 +0600

    Update load.js

--- Script/commands/load.js
+++ Script/commands/load.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "load",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴 ☢️",
 	description: "reload config file data",
 	commandCategory: "Admin",
 	usages: "[]",
